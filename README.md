44de59b2fba460befa5593d3e674646372fba1ab
baba_s <shotababa1206@gmail.com>
2020-05-04 12:11:52 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Uni Random Utils
+
+乱数に関する汎用的な関数を管理するクラス
+
+## 使用例
+
+```cs
+using UniRandomUtils;
+using UnityEngine;
+
+public sealed class Example : MonoBehaviour
+{
+    private void Start()
+    {
+        // 半径 1 の円の内部のランダムな点
+        Debug.Log( RandomUtils.InsideUnitCircle );
+
+        // 半径 1 の球体の内部のランダムな点
+        Debug.Log( RandomUtils.InsideUnitSphere );
+
+        // 半径 1 の球体の表面上にランダムな点
+        Debug.Log( RandomUtils.OnUnitSphere );
+
+        // ランダムな Quaternion 型の値
+        Debug.Log( RandomUtils.Rotation );
+
+        // ランダムな Quaternion 型の値
+        Debug.Log( RandomUtils.RotationUniform );
+
+        // 0.0 から 1.0 の間の浮動小数点数
+        Debug.Log( RandomUtils.Between0And1 );
+
+        // false か true
+        Debug.Log( RandomUtils.FalseOrTrue );
+
+        // 0 か 1
+        Debug.Log( RandomUtils.Value0Or1 );
+
+        var list = new[] { 1, 2, 3 };
+        
+        // 指定されたリストの中からランダムに要素を取得
+        Debug.Log( RandomUtils.RandomAt( list ) );
+        
+        // 0 から max - 1 の間の整数
+        Debug.Log( RandomUtils.Range( 100 ) );
+    }
+}
+```
---
+++ Scripts/RandomUtils.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace UniRandomUtils
+{
+	/// <summary>
+	/// 乱数に関する汎用的な関数を管理するクラス
+	/// </summary>
+	public static class RandomUtils
+	{
+		//================================================================================
+		// プロパティ(static)
+		//================================================================================
+		/// <summary>
+		/// 半径 1 の円の内部のランダムな点を返します
+		/// </summary>
+		public static Vector2 InsideUnitCircle => Random.insideUnitCircle;
+
+		/// <summary>
+		/// 半径 1 の球体の内部のランダムな点を返します
+		/// </summary>
+		public static Vector3 InsideUnitSphere => Random.insideUnitSphere;
+
+		/// <summary>
+		/// 半径 1 の球体の表面上にランダムな点を返します
+		/// </summary>
+		public static Vector3 OnUnitSphere => Random.onUnitSphere;
+
+		/// <summary>
+		/// ランダムな Quaternion 型の値を返します
+		/// </summary>
+		public static Quaternion Rotation => Random.rotation;
+
+		/// <summary>
+		/// ランダムな Quaternion 型の値を返します
+		/// </summary>
+		public static Quaternion RotationUniform => Random.rotationUniform;
+
+		/// <summary>
+		/// 0.0 から 1.0 の間の浮動小数点数をランダムに返します
+		/// </summary>
+		public static float Between0And1 => Random.value;
+
+		/// <summary>
+		/// false か true をランダムに返します
+		/// </summary>
+		public static bool FalseOrTrue => Random.Range( 0, 2 ) == 0;
+
+		/// <summary>
+		/// 0 か 1 をランダムに返します
+		/// </summary>
+		public static byte Value0Or1 => Convert.ToByte( FalseOrTrue );
+
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// シード値を指定し、ランダム数生成器の状態を初期化します
+		/// </summary>
+		public static void InitState( int seed )
+		{
+			Random.InitState( seed );
+		}
+
+		/// <summary>
+		/// 指定された配列の中からランダムに要素を返します
+		/// </summary>
+		public static T RandomAt<T>( params T[] values )
+		{
+			return values[ Random.Range( 0, values.Length ) ];
+		}
+
+		/// <summary>
+		/// 指定されたリストの中からランダムに要素を返します
+		/// </summary>
+		public static T RandomAt<T>( IList<T> values )
+		{
+			return values[ Random.Range( 0, values.Count ) ];
+		}
+
+		/// <summary>
+		/// 指定されたリストの中からランダムに要素を返します
+		/// </summary>
+		public static T RandomAt<T>( IReadOnlyList<T> values )
+		{
+			return values[ Random.Range( 0, values.Count ) ];
+		}
+
+		/// <summary>
+		/// 0 から max - 1 の間の整数をランダムに返します
+		/// </summary>
+		public static int Range( int max )
+		{
+			return Random.Range( 0, max );
+		}
+
+		/// <summary>
+		/// 0 から max - 1 の間の整数をランダムに返します
+		/// </summary>
+		public static byte RangeByte( byte max )
+		{
+			return ( byte ) Random.Range( 0, max );
+		}
+
+		/// <summary>
+		/// 0 から max - 1 の間の整数をランダムに返します
+		/// </summary>
+		public static ushort RangeUshort( uint max )
+		{
+			return ( ushort ) Random.Range( 0, ( int ) max );
+		}
+
+		/// <summary>
+		/// 0 から max - 1 の間の整数をランダムに返します
+		/// </summary>
+		public static uint RangeUint( uint max )
+		{
+			return ( uint ) Random.Range( 0, ( int ) max );
+		}
+
+		/// <summary>
+		/// 0 から max - 1 の間の整数をランダムに返します
+		/// </summary>
+		public static ulong RangeUlong( ulong max )
+		{
+			return ( ulong ) Random.Range( 0, ( int ) max );
+		}
+
+		/// <summary>
+		/// min から max - 1 の間の整数をランダムに返します
+		/// </summary>
+		public static int Range( int min, int max )
+		{
+			return Random.Range( min, max );
+		}
+
+		/// <summary>
+		/// min から max の間の浮動小数点数をランダムに返します
+		/// </summary>
+		public static float Range( float min, float max )
+		{
+			return Random.Range( min, max );
+		}
+	}
+}
